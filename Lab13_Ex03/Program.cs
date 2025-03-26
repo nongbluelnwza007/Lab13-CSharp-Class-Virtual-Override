113747f93ea078cffaa0c13bbc1e5d4904ffab4e
nongbluelnwza007 <booblueboyy@gmail.com>
2025-03-26 23:28:14 +0700

    add

---
+++ Lab13_Ex03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Animal
+{
+    public virtual void makeSound()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void makeSound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void makeSound()
+    {
+        Console.WriteLine("Cat meows");
+    }
+}
+
+public class AnimalSound
+{
+    public void PlaySound(Animal animal)
+    {
+        animal.makeSound();
+    }
+}
+
+// ใส่โค้ดทั้งหมดใน Main()
+class Program
+{
+    static void Main()
+    {
+        AnimalSound sound = new AnimalSound();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        sound.PlaySound(dog);
+        sound.PlaySound(cat);
+    }
+}
